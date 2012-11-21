5b12a78912c6db401ec4bc5f6d4a59f7e75e357d
unknown <US084134@US194NBO5SYZZL1.us.tycoelectronics.com>
2012-11-21 09:48:05 -0500

    initial

---
+++ AppHarborTest/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Raven.Client;
+using AppHarborTest.Raven;
+
+namespace AppHarborTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Welcome to ASP.NET MVC!";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Test()
+        {
+
+            using (IDocumentSession Session = DataDocumentStore.Instance.OpenSession())
+            {
+                string newId = Guid.NewGuid().ToString();
+                Session.Store(new TestModel
+                {
+                    Id = newId,
+                    Text = "punnnnnchy!"
+                });
+                Session.SaveChanges();
+
+                ViewBag.TestId = newId;
+
+            }
+
+            
+            return View();
+        }
+    }
+}
---
+++ AppHarborTest/Raven/TestModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppHarborTest.Raven
+{
+    public class TestModel
+    {
+        public string Id { get; set; }
+        public string Text { get; set; }
+    }
+}
